b23fffac00b739be3795ab4195ef348791241cbb
david <david@HP-470-DAVID>
2022-01-16 18:48:45 +0100

    modulo 3

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> user, SignInManager<ApplicationUser> sign)
+        {
+            _userManager = user;
+            _signInManager = sign;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
